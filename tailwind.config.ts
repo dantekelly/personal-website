83f1e046fbd618d00eb52cb69d6a47f3884ce2a6
Dante Kelly <dante@dantekelly.com>
2023-09-11 00:22:56 -0700

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,27 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ['node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}', "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  content: [
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
+    colors: {
+      slate: {
+        "50": "#F8FAFC",
+        "100": "#F1F5F9",
+        "200": "#E2E8F0",
+        "300": "#CBD5E1",
+        "400": "#94A3B8",
+        "500": "#64748B",
+        "600": "#475569",
+        "700": "#334155",
+        "800": "#1E293B",
+        "900": "#0F172A",
+      },
+    },
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require("flowbite/plugin")],
 } satisfies Config;
