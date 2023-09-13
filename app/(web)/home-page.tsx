147c243bc3209ebad3d091b8a172af0880437f66
Dante Kelly <dante@dantekelly.com>
2023-09-12 17:11:09 -0700

    Update meta data

--- app/(web)/home-page.tsx
+++ app/(web)/home-page.tsx
@@ -1,14 +1,7 @@
-import { type Metadata } from "next";
-
 import LayoutComponent from "~/components/layouts/layoutComponent";
 import Details from "~/components/sections/details";
 import Hero from "~/components/sections/hero";
 import Works from "~/components/sections/works";
-
-export const metadata: Metadata = {
-  title: "Dante Kelly - Portfolio",
-  description: "Personal Portfolio of Dante Kelly",
-};
 
 export default function Home() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
+import { type Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 
 import "~/styles/globals.css";
 
+export const metadata: Metadata = {
+  title: "Dante Kelly - Portfolio",
+  description: "Personal Portfolio of Dante Kelly",
+};
 export default function RootLayout({
   children,
 }: {
