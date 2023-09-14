63af8ff480a900887df5c9293ec4fcb4fb469820
Dante Kelly <dante@dantekelly.com>
2023-09-14 14:31:16 -0700

    Add 1hr max-cache to github api

--- app/utils/octokit.ts
+++ app/utils/octokit.ts
@@ -1,26 +1,32 @@
+import { cache } from "react";
+
 import client from "~/lib/octokit/client";
 
-export const getLastCommitDate = async (
-  repo: string,
-  owner: string,
-): Promise<string> => {
-  try {
-    const { data } = await client.request("GET /repos/{owner}/{repo}/commits", {
-      owner,
-      repo,
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    });
-    const lastCommit = data[0];
+export const revalidate = 3600; // revalidate the data at most every hour
 
-    if (!lastCommit?.commit?.committer?.date) {
-      throw new Error("No commit date found");
+export const getLastCommitDate = cache(
+  async (repo: string, owner: string): Promise<string> => {
+    try {
+      const { data } = await client.request(
+        "GET /repos/{owner}/{repo}/commits",
+        {
+          owner,
+          repo,
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        },
+      );
+      const lastCommit = data[0];
+
+      if (!lastCommit?.commit?.committer?.date) {
+        throw new Error("No commit date found");
+      }
+
+      return lastCommit.commit.committer.date;
+    } catch (error) {
+      console.error("Error fetching the last commit date:", error);
+      throw error;
     }
-
-    return lastCommit.commit.committer.date;
-  } catch (error) {
-    console.error("Error fetching the last commit date:", error);
-    throw error;
-  }
-};
+  },
+);
