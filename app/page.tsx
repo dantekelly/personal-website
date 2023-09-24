4d8efb56c248341aa5942ca46453fab040039a57
Dante Kelly <dante@dantekelly.com>
2023-09-24 13:04:24 -0700

    Remove old page

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// Import your Client Component
-import HomePage from "./(web)/home-page";
-
-export default function Page() {
-  // Forward fetched data to your Client Component
-  return <HomePage />;
-}
